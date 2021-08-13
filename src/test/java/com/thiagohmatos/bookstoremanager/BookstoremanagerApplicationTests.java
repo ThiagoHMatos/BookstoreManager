4ef1fd8fd19625a40ffaf1e292038df3f7b559c9
thiagohmatos <thiagohmatos@gmail.com>
2021-08-12 21:45:22 -0300

    first commit

---
+++ src/test/java/com/thiagohmatos/bookstoremanager/BookstoremanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiagohmatos.bookstoremanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoremanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
