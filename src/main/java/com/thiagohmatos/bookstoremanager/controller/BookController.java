0053993e986f85fb93e0abbd8f56b7dae9a2cf79
thiagohmatos <thiagohmatos@gmail.com>
2021-08-18 00:33:06 -0300

    including swager

--- src/main/java/com/thiagohmatos/bookstoremanager/controller/BookController.java
+++ src/main/java/com/thiagohmatos/bookstoremanager/controller/BookController.java
@@ -1,5 +1,8 @@
 package com.thiagohmatos.bookstoremanager.controller;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/books")
 public class BookController {
 
+    @ApiOperation("Return an exemple of hello word")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Success method return")
+    })
     @GetMapping
     public String hello(){
         return "Hello Bookstore Manager";
